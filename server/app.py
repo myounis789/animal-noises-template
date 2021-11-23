ed7f85c5f1f2a52bd4357b7d2396a670512330d9
Brxdpvrple <criley.dev@gmail.com>
2021-11-23 17:33:26 +0000

    commit on main

--- server/app.py
+++ server/app.py
@@ -3,7 +3,26 @@ from flask import Flask
 app = Flask(__name__)
 
 # home route here
+
+@app.route
+def home():
+
+    return 
+
 # must query the animal API for an animal and a noise – the noise should be based on the animal
+
+# MAKES A GET REQUEST FOR ANIMAL
+@app.route
+def animal_get():
+    pass
+
+
+
+# MAKES A POST REQUEST FOR ANIMAL NOISE
+@app.route('/noise')
+def noise_post():
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
