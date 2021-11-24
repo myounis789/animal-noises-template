a783345fc9acfccb115b2641d9f3b442e6609cfe
LordKalmed <angust.cloud@gmail.com>
2021-11-24 11:22:23 +0000

    Running example. Needs tests built

--- animal_api/app.py
+++ animal_api/app.py
@@ -1,30 +1,40 @@
 from flask import Flask, request, redirect
 from flask.helpers import url_for
+from flask.wrappers import Response
 import requests
 import random
+from random import randint
 
 app = Flask(__name__)
 
 # animal generator route here
-@app.route("/animal", methods=["GET"])
+@app.route("/animal/name", methods=["GET"])
 def animal():
     names = ["Cat", "Dog", "Pig", "Cow", "Lion"]
+    #or choice = animals[randint(0,2)]
     choice = random.choice(names)
-    return choice
+    return Response(choice, mimetype="text/plain")
 
 
 
 # animal noise generator route here
-@app.route("/animal", methods=["POST"])
+@app.route("/animal/noise", methods=["POST"])
 def noise():
     animal= request.data.decode('utf-8')
     noises = {"Cat": "meow", "Dog": "woof", "Pig": "oink", "Cow": "moow", "Lion": "Roar!"}
     choice = noises[animal].title()
-    return choice
+    return Response(choice, mimetype="text/plain")
+
+## alternative method ->
+    #if animal == "Cat"
+    #   noise == "meow"
+    #elif animal == "Dog"
+    #   noise == "Woof"
+    #return Response(noise, mimetype='ptext/plain')
 
 
     
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- server/app.py
+++ server/app.py
@@ -1,34 +1,35 @@
 from flask import Flask, render_template
-from flask.globals import request, Response
+from flask.globals import request
+import requests
 from jinja2.utils import pass_context
 
 app = Flask(__name__)
 
 # home route here
-
-@app.route("/")
-def home():
-    animal = request.get_data()
-    noise = request.get_data()
-    record= {1 : animal, 2 : noise}
-    return render_template("index.html", data = record )
+@app.route("/", methods=['GET'])
+def home_animal():
+    # http:// <container name> so that docker network will direct to 
+    animal = requests.get('http://api:5000/animal/name')
+    noise = requests.post('http://api:5000/animal/noise', data=animal.text)
+    #record = {1 : animal, 2 : noise}   Alternative way with json?
+    return render_template("index.html", animal=animal.text , noise=noise.text )
 
 # must query the animal API for an animal and a noise – the noise should be based on the animal
 
 # MAKES A GET REQUEST FOR ANIMAL
-@app.route("/http:xxxx/animal", methods=["GET"])
-def animal_get():
+#@app.route("/http:xxxx/animal", methods=["GET"])
+#def animal_get():
     #needs to get animal from api server
     animal= Response("Your animal is", mimetype="text/plain")
     #needs to get animal, and assign animal to correct noise.
     #return animal an noise.
 
 # MAKES A POST REQUEST FOR ANIMAL NOISE
-@app.route("/http:bbbb/animal", methods=["POST"])
+#@app.route("/http:bbbb/animal", methods=["POST"])
 
     #needs to get animal, and assign animal to correct noise.
     #return animal an noise.
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
